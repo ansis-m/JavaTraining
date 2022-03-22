adb2d635c92c95e74667595dd7a59ee1dc094f82
Ansis Maleckis <maleckisansis@gmail.com>
2022-03-22 11:41:50 +0200

    init

---
+++ untitled1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    boolean bool = true;
+        byte z = -120;
+        int x = 2;
+        float b = 1.234f;
+        double a = 1234.567890;
+        char v = 33;
+
+        System.out.println("boolean " + bool + "\nbyte " + z + "\nint " + x + "\nfloat " + b + "\ndouble " + a + "\nchar " + v);
+
+    }
+}
